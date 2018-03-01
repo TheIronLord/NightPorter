c1ce2c78b8b7c3c41a812489c4c79cdd7e291456
Sajjad Patel <sajjadp92@outlook.com>
2018-03-01 14:37:28 -0500

    UILabel text changes to light gray in Dark Mode

--- The Night Porter/The Night Porter/ViewController.swift
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -12,6 +12,14 @@ class ViewController: UIViewController {
 
     @IBAction func changeBackground(_ sender: Any) {
         view.backgroundColor = UIColor.darkGray
+        var everything = view.subviews
+        
+        for eachView in everything{
+            if eachView is UILabel{
+                let currentLabel = eachView as! UILabel
+                currentLabel.textColor = UIColor.lightGray
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
