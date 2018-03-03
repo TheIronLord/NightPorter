1b42578f270f8a41de06e3acbf38d2f47751e969
Sajjad Patel <sajjadp92@outlook.com>
2018-03-02 19:29:47 -0500

    Removed UILabels and put all the data into a UITableView Added sections and section headers into the UITableView Put all UILabel data into 3 arrays

--- The Night Porter/The Night Porter/ViewController.swift
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -8,19 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBAction func changeBackground(_ sender: Any) {
-        view.backgroundColor = UIColor.darkGray
-        var everything = view.subviews
-        
-        for eachView in everything{
-            if eachView is UILabel{
-                let currentLabel = eachView as! UILabel
-                currentLabel.textColor = UIColor.lightGray
-            }
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // Table View Delegate Methods
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected row \(indexPath.row) in section \(indexPath.section)")
+    }
+    
+    // Table View Data Source Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //Return the amount of sections in the table
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Returns the number of rows in each section
+        switch section {
+        case 0:
+            return dailyTasks.count
+        case 1:
+            return weeklyTasks.count
+        case 2:
+            return monthlyTasks.count
+        default:
+            return 0
         }
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Returns a UITableViewCell with the text from the specific indexPath section and row
+        let cell = UITableViewCell()
+        
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        case 2:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        default:
+            break
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // Returns the header for each section
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        case 2:
+            return "Monthly Tasks"
+        default:
+            return nil
+        }
+    }
+    
+    
+    // Create [String] arrays of tasks
+    let dailyTasks = ["Check all windows", "Check all doors", "Is the boiler fueled?",
+                      "Check the mailbox", "Empty trash containers", "If freezing check water pipes", "Document \"strange and unusual occurrences\" "]
+    
+    let weeklyTasks = ["Check inside all cabins", "Flush all lavatories in cabins",
+                       "Walk perimeter of property"]
+    
+    let monthlyTasks = ["Test security alarm", "Test motion detectors",
+                        "Test smoke alarms"]
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
