9025593c893676aa82c0512531d40b6ea918c604
Sajjad Patel <sajjadp92@outlook.com>
2018-03-05 14:34:39 -0500

    Update 3: Added Image, Subtitle, Assessory Type and Reusable Cell

--- The Night Porter/The Night Porter/ViewController.swift
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // Table View Delegate Methods
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Print out the row selected
         print("You selected row \(indexPath.row) in section \(indexPath.section)")
     }
     
     // Table View Data Source Methods
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         //Return the amount of sections in the table
         return 3
@@ -25,33 +25,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Returns the number of rows in each section
-        switch section {
-        case 0:
-            return dailyTasks.count
-        case 1:
-            return weeklyTasks.count
-        case 2:
-            return monthlyTasks.count
-        default:
-            return 0
-        }
+//        switch section {
+//        case 0:
+//            return dailyTasks.count
+//        case 1:
+//            return weeklyTasks.count
+//        case 2:
+//            return monthlyTasks.count
+//        default:
+//            return 0
+//        }
+        // Temporary return 5 rows
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Returns a UITableViewCell with the text from the specific indexPath section and row
-        let cell = UITableViewCell()
+//        let cell = UITableViewCell()
+//
+//        switch indexPath.section {
+//        case 0:
+//            cell.textLabel?.text = dailyTasks[indexPath.row]
+//        case 1:
+//            cell.textLabel?.text = weeklyTasks[indexPath.row]
+//        case 2:
+//            cell.textLabel?.text = weeklyTasks[indexPath.row]
+//        default:
+//            break
+//        }
+//        return cell
         
-        switch indexPath.section {
-        case 0:
-            cell.textLabel?.text = dailyTasks[indexPath.row]
-        case 1:
-            cell.textLabel?.text = weeklyTasks[indexPath.row]
-        case 2:
-            cell.textLabel?.text = weeklyTasks[indexPath.row]
-        default:
-            break
-        }
-        return cell
+        print("Calling cellForRowAtIndexPath for row: \(indexPath.row)")
+        
+        //Use reusable cell
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "normalCell", for: indexPath)
+        
+        // Add image, text, subtitle, and assessory type
+        myCell.textLabel?.text = "This is row number \(indexPath.row)"
+        myCell.imageView?.image = UIImage(named: "clock")
+        myCell.accessoryType = .disclosureIndicator
+        myCell.detailTextLabel?.text = "This is some detail text"
+        
+        return myCell
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
